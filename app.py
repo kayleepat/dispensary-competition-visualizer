5803f7c14f55949840580a736f794b6e4623f151
kayleepat <kayleepat@gmail.com>
2023-09-26 20:50:33 -0400

    backend api connection with flask

---
+++ app.py
@@ -0,0 +1,38 @@
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+import numpy as np
+import sqlite3
+from pathlib import Path
+import pandas as pd
+from flask import Flask, jsonify, render_template
+
+
+# from pathlib import Path
+from sqlalchemy import create_engine, inspect
+import pandas as pd
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///mmtc_dispensaries.sqlite")
+
+################################################
+# Flask Setup
+################################################
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    with engine.connect() as conn:
+        result = conn.execute('SELECT * FROM geolocated_dispensaries')
+
+        data = [dict(row) for row in result]   
+
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
